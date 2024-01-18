53f8380cdca56d87774b66b6652e7bbcaac7fe17
RuslanIlyasov <rus.ilyasov.2017@inbox.ru>
2024-01-18 13:23:10 +0300

    many chages

---
+++ task1.js
@@ -0,0 +1,21 @@
+let main_str = "Я изучаю JavaScript";
+//first task
+console.log(main_str[2], main_str.at(0), main_str[main_str.length - 1]);
+// second task
+let new_str = main_str.replace("ю", "л");
+console.log(new_str);
+new_str = main_str.split("JavaScript").join("Frontend");
+console.log(new_str);
+
+let p1 = '8'
+let p2 = 8
+
+if (p1 === p2) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+
+
+

e59c6c82a36b4c2f4c7723b5cb50b01faad50a6b
RuslanIlyasov <rus.ilyasov.2017@inbox.ru>
2024-01-19 12:57:58 +0300

    many chages

--- task1.js
+++ task1.js
@@ -6,16 +6,26 @@ let new_str = main_str.replace("ю", "л");
 console.log(new_str);
 new_str = main_str.split("JavaScript").join("Frontend");
 console.log(new_str);
-
-let p1 = '8'
-let p2 = 8
-
-if (p1 === p2) {
-    console.log(true);
-}
-else {
-    console.log(false);
-}
-
-
-
+//third task
+let new_mas = ["cat", "dog", "parrot", "horse"];
+hide_elem = new_mas.indexOf("parrot");
+console.log(hide_elem);
+//task 4
+let new_mas_task4_first = ["cat", "dog", "parrot", "horse", "fish", "chicken", "lion"];
+let new_mas_task4_second = new_mas_task4_first.concat();
+let new_mas_task4_third = new_mas_task4_first.concat();
+new_mas_task4_first.splice(4,1);
+console.log(new_mas_task4_first);
+new_mas_task4_second.shift();
+console.log(new_mas_task4_second);
+new_mas_task4_third.pop();
+new_mas_task4_third.pop();
+console.log(new_mas_task4_third);
+//task5
+let new_task5 = [31, 10, 'chicken', 9, 'fish', 10];
+let result_1 = new_task5.filter(function(item, index, array){
+    return typeof item === "string"
+});
+console.log(result_1);
+let result_2 = new_task5.filter(item => item === 10);
+console.log(result_2);
